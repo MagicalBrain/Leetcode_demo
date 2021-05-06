9c912a6c88187c840aced6623f1a186c64972c76
MagicalBrain <13542593649@163.com>
2021-05-06 21:05:02 +0800

    add 1720

---
+++ resource_code/1720.c
@@ -0,0 +1,19 @@
+/*
+ * 【leetcode】1720题代码 存档
+ * */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ * 注意：返回的数组必须分配内存空间，假设调用了free来释放空间。
+ */
+int* decode(int* encoded, int encodedSize, int first, int* returnSize){
+
+}
+
+int main() {
+    
+    return 0;
+}
